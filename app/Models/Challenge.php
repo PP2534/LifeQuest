0248f00e56bfcb45826c2d23cb3b3c117bcc9a44
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2025-11-10 20:38:15 +0700

    update function: +>Take the challenger. +>Get all the challenge participants. +>Get all the challenge Comment.

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Challenge extends Model
 {
@@ -31,10 +34,34 @@ class Challenge extends Model
     ];
 
     /**
-     * The categories that belong to the challenge.
+     * Lấy người tạo ra thử thách.
      */
-    public function categories()
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    /**
+     * Lấy các danh mục của thử thách.
+     */
+    public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class, 'challenge_categories');
+    }
+
+    /**
+     * Lấy tất cả người tham gia thử thách.
+     */
+    public function participants(): HasMany
+    {
+        return $this->hasMany(ChallengeParticipant::class);
+    }
+
+    /**
+     * Lấy tất cả bình luận của thử thách.
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
