dcbec0d5a95d00b031573fb856f7ea2dc4ff0dac
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2025-11-22 14:03:41 +0700

    update button roll call

--- resources/views/livewire/challenges/challenge-detail.blade.php
+++ resources/views/livewire/challenges/challenge-detail.blade.php
@@ -62,7 +62,6 @@
                     <div id="progress-fill" class="bg-teal-600 h-6 rounded-full transition-width duration-1000 ease-in-out" 
                          style="width: {{ $myParticipation->progress_percent }}%;">
                     </div>
-                    
                     <span class="absolute right-3 top-0 text-white font-semibold text-sm leading-6">
                         {{ $myParticipation->progress_percent }}%
                     </span>
@@ -73,6 +72,15 @@
                         Chuỗi {{ $myParticipation->streak }} ngày 
                         </div>
                 @endif
+                <div class="mt-6 flex justify-center">
+                    <a href="{{ route('challenges.checkin', $challenge) }}" wire:navigate
+                        class="inline-flex items-center px-6 py-3 bg-teal-600 hover:bg-teal-700 text-white font-bold rounded-lg shadow-lg transform transition hover:scale-105">
+                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                        </svg>
+                        Điểm danh & Xem Lịch
+                    </a>
+                </div>
             </section>
         @endif
 
