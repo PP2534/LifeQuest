92e158bd177160bb5bde647d32e4e728b9226b8d
PP2354 <phanphuong.hnq2014@gmail.com>
2025-12-03 21:42:26 +0700

    fix bug 3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { fileURLToPath, URL } from 'node:url';
+
+const resolveFromRoot = (relativePath) => fileURLToPath(new URL(relativePath, import.meta.url));
 
 export default defineConfig({
     resolve: {
         alias: {
-            'laravel-echo': 'laravel-echo/dist/echo.js',
-            'pusher-js': 'pusher-js/dist/web/pusher.js',
+            'laravel-echo': resolveFromRoot('./node_modules/laravel-echo/dist/echo.js'),
+            'pusher-js': resolveFromRoot('./node_modules/pusher-js/dist/web/pusher.js'),
         },
     },
     plugins: [
